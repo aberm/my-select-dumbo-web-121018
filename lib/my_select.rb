ae42a2c78f3992a7918da0cf70598bb0b7cfe2eb
Menachem Aber <abermendel@gmail.com>
2018-11-26 19:07:04 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(collection)
   i = 0
   collection2 = []
   while i < collection.length
-    collection2.push(if yield collection[i])
+    collection2 << if yield collection[i]
     i += 1
   end
   collection2
